80dd37eb5a201be85340a9becdfa02060a17f1ae
CursoJAVA <CursoJAVA@Usuario-PC.home>
2019-01-14 19:12:10 +0100

    Añadido el boton faltante al ejercicio 6

--- EjerciciosJquery/WebContent/js/slideshow.js
+++ EjerciciosJquery/WebContent/js/slideshow.js
@@ -1,29 +1,41 @@
 $(document).ready(function(){
-	$(document).ready(function(){
         $("#boton1").click(function(){
         	
         	 $('body').prepend( "<ul id='slideshow2'>" );
         	 $('#slideshow').appendTo('#slideshow2');
         });
+        
+        $("#boton2").click(function(){
+  	      var $p=$("#slideshow > li");
+  	      var tamano=$p.length;
+  	      $p.hide();
+          
+  	      $p.first();
+  	      var cont=0;
+            setInterval(function(){
+          	  $p.eq(cont).fadeIn(1000);
+          	  $p.eq(cont).fadeOut(1000);
+          	  cont++;
+          	  if(cont>=tamano){
+          		  cont=0;
+          	  }
+            }, 2000);
     });
-  
-  
-  $(document).ready(function(){
-      $("#boton2").click(function(){
-    	      var $p=$("#slideshow > li");
-    	      var tamano=$p.length;
-    	      $p.hide();
-            
-    	      $p.first();
-    	      var cont=0;
-              setInterval(function(){
-            	  $p.eq(cont).fadeIn(1000);
-            	  $p.eq(cont).fadeOut(1000);
-            	  cont++;
-            	  if(cont>=tamano){
-            		  cont=0;
-            	  }
-              }, 2000);
-      });
-  });
+
+        $("#boton3").click(function(){
+        clearInterval(myVar);
+        
+  	      var $p=$("#slideshow > li");
+  	      var tamano=$p.length;
+  	      
+  	      $p.hide();
+          $p.eq(contador).fadeIn(1000);
+          
+          contador++;
+          
+          if(contador>=tamano){
+        	  contador=0;
+          }
+    });
+        
 });
