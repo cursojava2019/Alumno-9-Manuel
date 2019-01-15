d006c91d2a483c3ef7b40a659b2ca0b3b0be94ef
CursoJAVA <CursoJAVA@192.168.1.10>
2019-01-15 20:38:15 +0100

    Implementado clientes

--- EjerciciosBanco1/src/es/indra/model/Cuentas.java
+++ EjerciciosBanco1/src/es/indra/model/Cuentas.java
@@ -1,5 +1,19 @@
 package es.indra.model;
 
 public class Cuentas {
+	private Double saldo;
+	private Clientes cliente;
+	private String tipoDeCuenta;
+
+	public Cuentas(Double saldo, Clientes cliente, String tipoDeCuenta) {
+		super();
+		this.saldo = saldo;
+		this.cliente = cliente;
+		this.tipoDeCuenta = tipoDeCuenta;
+	}
+
+	public Cuentas() {
+		super();
+	}
 
 }
